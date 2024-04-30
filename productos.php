e57ef44116de9920d2007668bfa09c0dbb663a4d
Breyson Cc <breyson567@gmail.com>
2024-04-30 10:42:45 -0500

    Actualizando la página
    agregamos los productos a la página de productos.php

--- productos.php
+++ productos.php
@@ -262,17 +262,18 @@
                             </li>
 
                             <li>
-                                <a href="productos.php" class="ui-buttons.html"><i
+                                <a href="javascript: void(0);" class="has-arrow waves-effect"><i
                                         class="mdi mdi-menu"></i><span>Celulares</span></a>
+                                
                             </li>
 
                             <li>
-                                <a href="javascript: void(0);" class="ui-buttons.html"><i
+                                <a href="javascript: void(0);" class="has-arrow waves-effect"><i
                                         class="mdi mdi-tag"></i><span>Ofertas</span></a>
                             </li>
 
                             <li>
-                                <a href="javascript: void(0);" class="ui-buttons.html"><i
+                                <a href="javascript: void(0);" class="has-arrow waves-effect"><i
                                         class="mdi mdi-star-circle"></i><span>Lanzamientos</span></a>
                             </li>
 
@@ -283,27 +284,29 @@
                             </li>
 
                             <li>
-                                <a href="javascript: void(0);" class="ui-buttons.html"><i
+                                <a href="javascript: void(0);" class="has-arrow waves-effect"><i
                                         class="mdi mdi-truck"></i><span>Pedidos</span></a>
+            
                             </li>
 
                             <li class="menu-title">Más Opciones</li>
 
                             <li>
-                                <a href="javascript: void(0);" class="ui-buttons.html"><i
+                                <a href="javascript: void(0);" class="has-arrow waves-effect"><i
                                         class="mdi mdi-maxcdn"></i><span>Marcas</span></a>
+        
                             </li>
 
                             <li><a href="calendar.html" class=" waves-effect"><i
                                         class="mdi mdi-cart"></i><span>Carrito de Compras</span></a></li>
 
                             <li>
-                                <a href="javascript: void(0);" class="ui-buttons.html"><i
+                                <a href="javascript: void(0);" class="has-arrow waves-effect"><i
                                         class="mdi mdi-map-marker-radius"></i><span>Ubicaciones</span></a>
                             </li>
 
                             <li>
-                                <a href="javascript: void(0);" class="ui-buttons.html"><i
+                                <a href="javascript: void(0);" class="has-arrow waves-effect"><i
                                         class="mdi mdi-message-text-outline"></i><span>Atencion al Cliente</span></a>
                             </li>
 
@@ -326,50 +329,28 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-flex align-items-center justify-content-between">
-                                    <h4 class="mb-0 font-size-18"> Ofertas</h4>
-
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Página</a></li>
-                                            <li class="breadcrumb-item active">Inicio</li>
-                                        </ol>
+                                    <div class="container-fluid row">
+                                        <?php for($i=0; $i < 10; $i++) {
+                                            ?>
+                                        <div class="col-md-3">
+                                            <div class="card">
+                                                <img class="card-img-top img-fluid" src="img/cel3.jpeg">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Titulo</h5>
+                                                    <p class="card-text">
+                                                        Descripcion
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php } ?>
                                     </div>
 
                                 </div>
                             </div>
                         </div>
                         <!-- end page title -->
-
                     </div> <!-- container-fluid -->
-                    <!-- INICIO CARRUSEL -->
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="img/16243123227814.jpg" class="d-block mx-auto img-fluid" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img/2da.jpeg" class="d-block mx-auto img-fluid" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img/3ra.jpg" class="d-block mx-auto img-fluid" alt="...">
-                            </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                    <!-- FIN CARRUSEL -->
-
                 </div>
                 <!-- End Page-content -->
 
