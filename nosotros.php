e01615c76dae267c132ac49f56767a0cf7bdd533
Breyson Cc <breyson567@gmail.com>
2024-05-16 09:12:16 -0500

    menu nosotros
    actualizando el menú nosotros.php

--- nosotros.php
+++ nosotros.php
@@ -39,7 +39,7 @@
 
                     <div class="row">
                         <div class="col-sm-12 text-center">
-                            <h1 style="color:orange">Sobre <span style="color:black">Nosotros</span></h1>
+                            <i><h1 style="color:orange">Sobre <span style="color:black">Nosotros</span></h1></i>
                         </div>
                     </div><!-- end col -->
                     <hr>
@@ -54,16 +54,16 @@
                                     Nuestra historia se inicia en el año 2005 con la venta de celulares en una tienda pequeña en la región de Ayacucho.
                                 </p>
                                 <p>
-                                    En el año 2010 pasamos a formar parte del grupo <strong>"PROMART"</strong>, en ese momento contábamos con 8 tiendas ubicadas
+                                    En el año 2010 pasamos a formar parte del grupo <strong style="color:orange">"PROMART"</strong>, en ese momento contábamos con 8 tiendas ubicadas
                                     en 4 ciudades del Perú. Gracias al deseo de nuestra gente, fuimos creciendo y en el 2018, ya contábamos con 10 tiendas.
                                 </p>
                                 <p>
-                                    Nuestra empresa se caracteriza por la venta de celulares de la marca Xiaomi.
-                                    Nuestra tienda está conformada por profesionales altamente capacitados y responsables y que saben
-                                    lo que te gusta.
+                                    Nos distinguimos por ser especialistas en la venta de celulares de la reconocida marca Xiaomi, ofreciendo a nuestros clientes 
+                                    productos de alta calidad y tecnología de vanguardia. contamos con un equipo de profesionales 
+                                    altamente capacitados y comprometidos, quienes se esfuerzan día a día para brindarte la mejor experiencia de compra y asesoramiento personalizado.
                                 </p>
                                 <p>
-                                    Somos una empresa 100% peruano que apuesta por el país.
+                                    Somos una empresa 100% peruana, arraigada en nuestra cultura y comprometida con el desarrollo y el bienestar de nuestro país.
                                 </p>
                                 <br>
                                 <h2>Nuestro Compromiso</h2>
@@ -71,6 +71,7 @@
                                 <p><i class="fas fa-star"></i> 1 año de garantía en todos nuestros productos</p>
                                 <p><i class="fas fa-star"></i> Te ofrecemos los mejores precios del mercado todos los días</p>
                                 <p><i class="fas fa-star"></i> Te brindamos el mejor servicio con amabilidad</p>
+                                <p><i class="fas fa-star"></i> Te ofrecemos productos de buena calidad sellados</p>
                             </div>
                         </div>
                     </div>
